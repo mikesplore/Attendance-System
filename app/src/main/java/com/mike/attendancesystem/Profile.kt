36c99b91347f6fd751f3947c388f5239bce307ac
mikesplore <mikepremium8@gmail.com>
2024-05-15 13:01:54 +0300

    initial commit

--- app/src/main/java/com/mike/attendancesystem/Profile.kt
+++ app/src/main/java/com/mike/attendancesystem/Profile.kt
@@ -1,2 +1,117 @@
 package com.mike.attendancesystem
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun StudentProfile() {
+Column(modifier = Modifier
+    .background(brush)
+    .fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(modifier = Modifier
+            .width(350.dp)
+            .height(200.dp)
+            .background(color = Color(0xff00A9FF), shape = RoundedCornerShape(20.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly) {
+            Image(painter = painterResource(id = R.drawable.teacher), contentDescription = "dp",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(100.dp))
+
+        }
+        Column(modifier = Modifier
+            .height(400.dp)
+            .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceEvenly,) {
+            Row(modifier = Modifier
+
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                SquareBox(imageName = painterResource(id = R.drawable.teacher), content = "Timetable")
+                SquareBox(imageName = painterResource(id = R.drawable.teacher), content = "Attendance")
+
+            }
+            Row(modifier = Modifier
+
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                SquareBox(imageName = painterResource(id = R.drawable.teacher), content = "Announcements")
+                SquareBox(imageName = painterResource(id = R.drawable.teacher), content = "Resources")
+
+            }
+        }
+    }
+}
+@Composable
+fun SquareBox(imageName: Painter, content: String) {
+    val image: Painter = imageName
+    Box(
+        modifier = Modifier
+            .shadow(
+                elevation = 15.dp,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .background(color = Color(0xffA0E9FF))
+            .size(150.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            Column(modifier = Modifier
+                .size(90.dp)
+                .background(color = Color(0xffCDF5FD), shape = CircleShape),) {
+
+            Image(
+                painter = image,
+                contentDescription = "box content",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(90.dp)
+        )}
+        Text(
+            text = content,
+            color = Color.Black,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+
+
+
+        )
+        }
+    }
+
+}
+
+
+
+@Preview
+@Composable
+fun StudentProfilePreview() {
+    StudentProfile()
+}
