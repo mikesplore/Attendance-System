a7974213bc21fa878ef05e14216d996a8950e4cc
mikesplore <mikepremium8@gmail.com>
2024-05-15 17:09:28 +0300

    initial commit

--- app/src/main/java/com/mike/attendancesystem/Profile.kt
+++ app/src/main/java/com/mike/attendancesystem/Profile.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,9 +20,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -55,55 +56,138 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Student(navController: NavController){
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+fun Student(navController: NavController) {
+    // Define drawer state and coroutine scope
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
+    // Modal navigation drawer for the side menu
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
+                // Content for the side menu
                 Column(
                     modifier = Modifier
                         .fillMaxHeight()
                         .width(270.dp)
                         .background(color = Color(0xffA0E9FF)),
-                    verticalArrangement = Arrangement.SpaceEvenly,
+                    verticalArrangement = Arrangement.SpaceBetween,
                     horizontalAlignment = Alignment.CenterHorizontally
-
-                ){
-
-
+                ) {
+                    // Content of the drawer
+                    Column(
+                        modifier = Modifier
+                            .background(Color(0xff89CFF3), shape = RoundedCornerShape(10.dp))
+                            .height(250.dp)
+                            .fillMaxWidth()
+                    ) {
+                        // Back button in the top right corner
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.CenterEnd
+                        ) {
+                            Icon(
+                                Icons.Filled.ArrowBack,
+                                contentDescription = "Back",
+                                modifier = Modifier
+                                    .clickable {
+                                        // Handle back button click to close the drawer
+                                        scope.launch {
+                                            drawerState.apply {
+                                                if (isClosed) open() else close()
+                                            }
+                                        }
+                                    }
+                                    .padding(8.dp)
+                            )
+                        }
+                        // Image and name in the middle of the drawer
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Column(
+                                modifier = Modifier.height(200.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.teacher),
+                                    contentDescription = "dp",
+                                    modifier = Modifier
+                                        .clip(CircleShape)
+                                        .size(70.dp)
+                                )
+                                Text(
+                                    "Michael Odhiambo",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black,
+                                )
+                                Text(
+                                    "BSCS/108J/2021",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black,
+                                )
+                            }
+                        }
+                    }
+                    Row (modifier = Modifier
+                        .absolutePadding(0.dp,0.dp,0.dp,10.dp)
+                        .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                        ){
+                        Icon(imageVector = Icons.Outlined.ExitToApp, contentDescription = "Logout")
+                        Text(text = "Logout",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color.Black,
+                            modifier = Modifier.clickable{
+                                navController.navigate("StudentLogin")
+                            })
+                    }
                 }
-
-
             }
-        },
-        ) {
+        }
+    ) {
+        // Scaffold for the main content
         Scaffold(
             topBar = {
+                // Top app bar
                 TopAppBar(
                     title = { Text(text = "Student Dashboard") },
                     navigationIcon = {
+                        // Menu icon to open/close the drawer
                         IconButton(onClick = {
-                            // Handle navigation icon click
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
                         }) {
-                            Icon(Icons.Filled.Menu, contentDescription = "Menu",
+                            Icon(
+                                Icons.Filled.Menu,
+                                contentDescription = "Menu",
                                 modifier = Modifier.clickable {
+                                    // Handle click to open/close the drawer
                                     scope.launch {
                                         drawerState.apply {
                                             if (isClosed) open() else close()
                                         }
                                     }
-
-
-                                })
+                                }
+                            )
                         }
                     },
+                    // Set top app bar colors
                     colors = TopAppBarDefaults.smallTopAppBarColors(Color(0xffA0E9FF))
                 )
-
             },
             content = {
+                // Main content of the screen
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
@@ -111,14 +195,14 @@ fun Student(navController: NavController){
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Spacer(modifier = Modifier.height(70.dp)) // Height of the top app bar
-                    HeaderContent()
-                    BoxContent(navController)
-
+                    HeaderContent() // Content for the header
+                    BoxContent(navController) // Content for the main section
                 }
             }
         )
     }
 }
+
 
 @Composable
 fun HeaderContent() {
